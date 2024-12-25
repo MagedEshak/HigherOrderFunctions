d9b5f110ab79bf9e68b7b3085e6a77c235d18349
Maged Eshak <magedeshak39@gmail.com>
2024-12-25 11:39:06 +0200

    first commit

---
+++ HigherOrderFunctions.js
@@ -0,0 +1,35 @@
+console.log("1- Higher Order Functions – Map");
+console.log("-".repeat(20));
+
+
+console.log("-".repeat(20));
+console.log("2- Higher Order Functions – Map Practice");
+console.log("-".repeat(20));
+
+
+console.log("-".repeat(20));
+console.log("3- Higher Order Functions – Filter");
+console.log("-".repeat(20));
+
+
+console.log("-".repeat(20));
+console.log("4- Higher Order Functions – Filter Practice");
+console.log("-".repeat(20));
+
+
+
+console.log("-".repeat(20));
+console.log("5- Higher Order Functions – Reduce");
+console.log("-".repeat(20));
+
+
+
+console.log("-".repeat(20));
+console.log("6- Higher Order Functions – Reduce Practice");
+console.log("-".repeat(20));
+
+
+
+console.log("-".repeat(20));
+console.log("7- Higher Order Functions – ForEach & Practice");
+console.log("-".repeat(20));
