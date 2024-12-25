a6371b7fa85b789c1f0cf484058efff2cf5482f6
Maged Eshak <magedeshak39@gmail.com>
2024-12-25 14:10:50 +0200

    update

--- HigherOrderFunctions.js
+++ HigherOrderFunctions.js
@@ -1,10 +1,44 @@
 console.log("1- Higher Order Functions – Map");
 console.log("-".repeat(20));
 
+let myNums = [2, 4, 6, 8, 10];
+let nums = [];
+for (let i = 0; i < myNums.length; i++) {
+    nums.push(myNums[i] + myNums[i]);
+}
+console.log(nums);
+
+// map
+
+let addSelf = myNums.map(function (element, index, arr) {
+    
+    console.log(`element ${element}`);
+    console.log(`index ${index}`);
+    console.log(`Array ${arr}`);
+    console.log(`this ${this}`);
+    return element + element;
+},10);
+    console.log(addSelf);
+
+console.log(`Arrow Fun
+${"-".repeat(20)}`); 
+
+let addSlf = myNums.map((e) => e + e);
+console.log(addSelf);
+
+console.log(` Fun
+${"-".repeat(20)}`); 
+function addition(e) {
+    return e + e;
+}
+
+let add = myNums.map(addition);
+console.log(add);
 
 console.log("-".repeat(20));
 console.log("2- Higher Order Functions – Map Practice");
 console.log("-".repeat(20));
+
 
 
 console.log("-".repeat(20));
