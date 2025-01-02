25acf904b91c3c01ddc9256b0cbab5ac93bc346c
Maged Eshak <magedeshak39@gmail.com>
2025-01-02 16:40:30 +0200

    up

--- assigmentHigherOrderFunctions.js
+++ assigmentHigherOrderFunctions.js
@@ -33,7 +33,21 @@ console.log("----------------------");
 console.log("-- التكليف 03 --");
 console.log("----------------------");
 
+// My Solution without use Reduce fun
+console.log("-- My Solution without use Reduce fun --");
 
+let myArray = ["E", "l", "z", ["e", "r"], "o"];
+let flattenArray = myArray.join("").split("").map(ele => ele !== "," ? ele : "").join("");
+console.log(flattenArray); // Elzero
+
+// My Solution with use Reduce fun
+console.log("-- My Solution with use Reduce fun --");
+
+let flattennArray = myArray.reduce(function (acc, current) {
+    return acc.concat(current);
+}, []).join("");
+
+console.log(flattennArray);  // Output: Elzero
 
 console.log("----------------------");
 console.log("-- التكليف 04 --");
@@ -57,3 +71,32 @@ let finalNum = numbs.reduce(function (accumulator, current) {
     return current % 2 === 0 ? accumulator * current : accumulator + current;
 }, 1);
 console.log(finalNum); // 500
+
+console.log("----------------------");
+console.log("-- Higher Order Functions Challenges --");
+console.log("----------------------");
+
+/*
+    Higher Order Functions Challenges
+
+    You Can Use
+    - ,
+    - _
+    - Space
+    - True => 1 => One Time Only In The Code
+
+    You Cannot Use
+    - Numbers
+    - Letters
+
+    - You Must Use [Filter + Map + Reduce + Your Knowledge]
+    - Order Is Not Important
+    - All In One Chain
+
+*/
+
+let myStrin = "1,2,3,EE,l,z,e,r,o,_,W,e,b,_,S,c,h,o,o,l,2,0,Z";
+
+let solution = myStrin;
+
+console.log(solution); // Elzero Web School
